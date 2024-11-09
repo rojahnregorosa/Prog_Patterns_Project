6f99c6cc5ce6bf92faeb09484bfd895c3fe3d643
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-09 16:36:25 -0500

    Added lombok, made changed to User and Member class

--- GymSystem/src/main/java/org/example/model/Member.java
+++ GymSystem/src/main/java/org/example/model/Member.java
@@ -1,4 +1,24 @@
 package org.example.model;
 
-public class Member {
+import lombok.Getter;
+
+@Getter
+public class Member extends User {
+    protected static int memberIdCounter = 0;
+    protected int memberId; //maybe string
+    protected Membership membershipType;
+    protected double balance;
+
+    public Member(String name, Address address, String phoneNumber) {
+        super(name, address, phoneNumber);
+    }
+
+    public Member(String name, Address address, String phoneNumber, Membership membershipType, double balance) {
+        super(name, address, phoneNumber);
+        this.memberId = memberIdCounter++;
+        this.membershipType = membershipType;
+        this.balance = balance;
+    }
+
+
 }
--- GymSystem/src/main/java/org/example/model/User.java
+++ GymSystem/src/main/java/org/example/model/User.java
@@ -1,4 +1,13 @@
 package org.example.model;
 
-public class User {
+public abstract class User {
+    protected String name;
+    protected Address address;
+    protected String phoneNumber;
+
+    public User(String name, Address address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
 }
